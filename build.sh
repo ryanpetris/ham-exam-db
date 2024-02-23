8ffd511e9e199f3c6ec7f34e663066e0bbab8140
Ryan Petris <ryan@petris.net>
2024-02-23 16:32:08 -0700

    Add better error messages

--- build.sh
+++ build.sh
@@ -18,13 +18,22 @@ dep_setup() {
 
 ./run.sh dep-check 2>/dev/null || dep_setup
 
-mapfile -t SOURCES < <(ls "${SOURCE_DIR}")
+mkdir -p "${SOURCE_DIR}"
 mkdir -p "${OUT_DIR}"
+mapfile -t SOURCES < <(ls "${SOURCE_DIR}")
+
+if [ "${#SOURCES[@]}" -eq 0 ]; then
+    echo "Could not find question pools. Download docx version of question pools from https://ncvec.org/ and place in source directory." >&2
+    exit 1
+fi
 
 for SOURCE in "${SOURCES[@]}"; do
     DB_NAME="${SOURCE%.*}.db"
 
-    echo "Processing ${SOURCE}..."
+    echo "Processing ${SOURCE}..." >&2
 
     ./run.sh parse "${SOURCE_DIR}/${SOURCE}" "${OUT_DIR}/${DB_NAME}"
 done
+
+echo "" >&2
+echo "Processing complete. Check dist directory for output." >&2
